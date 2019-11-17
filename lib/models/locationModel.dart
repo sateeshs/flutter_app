c8d1fa78d45539ce124945c6d2ac9fcb65d1851a
ssateesh <sateesh.sabbineni@hotmail.com>
2019-11-17 16:37:30 -0500

    Added Location search and navigate to that city

---
+++ lib/models/locationModel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Location extends StatefulWidget{
+  final String title;
+  Location({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
---
+++ lib/state/appState.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/models/locationModel.dart';
+
+/*class AppState extends State<Location>
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+*/
