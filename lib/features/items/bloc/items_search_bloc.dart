c312a055f49f99ef524971cfd43aa33b35344540
sateesh sabbineni <sateesh.sabbineni@hotmail.com>
2019-11-26 23:53:21 -0500

    working on UI error

--- lib/features/items/bloc/items_search_bloc.dart
+++ lib/features/items/bloc/items_search_bloc.dart
@@ -1,11 +1,28 @@
 import 'dart:async';
 
+import 'package:bloc/bloc.dart';
+
+import 'item_search_event.dart';
+import 'item_search_state.dart';
+
 class ItemSearchBloc
     extends Bloc<ItemSearchEvent, ItemSearchState> {
-  final FacilitiesService facilitiesService;
-  final LocationsSearchBloc locationsSearchBloc;
+ // final FacilitiesService facilitiesService;
+  //final LocationsSearchBloc locationsSearchBloc;
   StreamSubscription locationSearchSubscription;
+    StreamSubscription fecilitySearchSubscription;
+
 
   ItemSearchBloc();
+
+  @override
+  // TODO: implement initialState
+  ItemSearchState get initialState => ItemSearchStateEmpty();
+
+  @override
+  Stream<ItemSearchState> mapEventToState(ItemSearchEvent event) {
+    // TODO: implement mapEventToState
+    return null;
+  }
 
     }
