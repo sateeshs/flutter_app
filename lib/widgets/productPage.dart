abf0b676dac9b726fa8a487d99ad37c967ed313e
sateesh sabbineni <sateesh.sabbineni@hotmail.com>
2019-12-02 23:22:08 -0500

    working on listview

--- lib/widgets/productPage.dart
+++ lib/widgets/productPage.dart
@@ -2,47 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/models/locationModel.dart';
 class ProductPage extends StatelessWidget {
   final LocationModel product;
-
+  List<String> items = ["Beer", "Food", "Movie"];
   ProductPage({this.product});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(title: new Text(this.product.title,
-        style: Theme.of(context).textTheme.headline,),),
+      appBar: new AppBar(
+        title: new Text(
+          this.product.title,
+          style: Theme.of(context).textTheme.headline,
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(50.0),
-        child: Column(
-          children: [
-         _myListView(context),//List of features
-//ActionButtons()
-          ],
+        child: FutureBuilder(
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            return _myListView(context); //List of features
+ }
+
         ),
       ),
     );
-  }
+  } //build
 
   ListView _myListView(BuildContext context) {
-    return ListView(
+    return ListView.builder(
       physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        ListTile(
-          leading: Icon(Icons.fastfood),
-          title: Text('Food'),
-        ),
-        ListTile(
-          leading: Icon(Icons.local_drink),
-          title: Text('Beer'),
-          onTap: (){
-            Navigator.push(context,null //new MaterialPageRoutebuilder: (context) => new NowPlaying(songData, s)))
-                         );
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.movie),
-          title: Text('Movie'),
-        ),
-      ],
+      itemCount: items.length,
+      itemBuilder: (context, index) => buildItem(context, items[index]),
+    );
+  }
+
+  ListTile buildItem(BuildContext context, String item) {
+    return ListTile(
+      leading: Icon(Icons.local_drink),
+      title: Text(item),
+      onTap: () {
+        Navigator.push(context,null
+          //new MaterialPageRoutebuilder: (context) => new NowPlaying(songData, s)
+        );
+      },
     );
   }
 }
